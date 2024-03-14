c359f2e371a1c7e9ec715dbf449f3b1b62dc20dc
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-14 18:43:15 +0000

    duck type

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# The types of the elements of the input are not know
+
+
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    Return the first element of a sequence safely.
+    Parameters:
+        lst(Sequence[Any]): The Input sequence can be any type
+
+    Returns:
+        Union[Any, None]: The first element of the sequence or None
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
