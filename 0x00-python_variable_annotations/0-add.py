9c622ff26ce6a6806f6b5a54544bc09369ab7187
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-14 09:31:23 +0000

    pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 '''Type Annotations for Function Parameters and Return Values'''
+
+
 def add(a: float, b: float) -> float:
     """
     Adds two floating-point numbers and returns the result.
@@ -7,7 +9,7 @@ def add(a: float, b: float) -> float:
     Parameters:
         a (float): The first floating-point number to be added.
         b (float): The second floating-point number to be added.
-    
+
     Return:
         float: the sum of the two input floating-point numbers.
     """
